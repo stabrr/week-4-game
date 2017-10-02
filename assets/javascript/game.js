172d0dd34b838802e23c057f7644a47ccb203dd1
stabrr <bobbystabile@yahoo.com>
2017-10-02 17:01:35 -0500

    Clean Up code and comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,59 +6,62 @@ var winNum = 0;
 var lossNum = 0;
 //random value for crsystal 1-12
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//location of pictures
 var picture = "assets/images/";
-var newNumbers = numberOptions.sort(function() { return 0.5 - Math.random() });
-console.log(newNumbers);
+//used for random shuffled crystal value numbers
+var newNumbers = [];
 
 
+//random target number btwn 19-120
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+//this will take the shuffled array and asign values to the crystals
  function assignCrystalValue (changeNumbers){
-//  var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-//  var newNumbers = numberOptions.sort(function() { return 0.5 - Math.random() });
   console.log(changeNumbers);
   for (var i = 1; i < 5; i++){
-    //imageCrystal.attr("data-crystalvalue", changeNumbers[i]);
     ($(".crystal-image" + i).attr("data-crystalvalue",changeNumbers[i]));
     console.log("chnumi: " + changeNumbers[i]);
   }
-
+ }
+//this starts the game and resets the game by getting a target number and the new values for the crystals
+ function gameReset (){
+      targetNumber = getRandomInt (19,120);
+      newNumbers = numberOptions.sort(function() { return 0.5 - Math.random() }); 
+      assignCrystalValue(newNumbers);
+      console.log(newNumbers); 
+      console.log("imageCrystal: " + imageCrystal);  
+      counter=0;   
+      $("#number-to-guess").text(targetNumber);
+      $("#your-score").text(counter);
  }
 
-   targetNumber = getRandomInt (19,120);
-   $("#number-to-guess").text(targetNumber);
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 1; i < 5; i++) {
-
+  // Next we create a for loop to create 4 crystals and two classes one style one for value
+    for (var i = 1; i < 5; i++) {
     // For each iteration, we will create an imageCrystal
     var imageCrystal = $("<img>");
 
     // First each crystal will be given the class ".crystal-image".
     // This will allow the CSS to take effect.
     imageCrystal.addClass("crystal-image");
-
+    //Class created with a number to represent each crystal
+    //this is needed to assign a vakue to each crystal
     imageCrystal.addClass("crystal-image"+i);
 
     // Each imageCrystal will be given a src link to the crystal image
     imageCrystal.attr("src",picture + i+".jpg");
 
-
-
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", newNumbers[i]);
-
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
     $("#crystals").append(imageCrystal);
     ("imageCrystal: " + imageCrystal); 
-    console.log("imageCrystal: " + imageCrystal);
+    
   }
  
 
   // This time, our click event applies to every single crystal on the page. Not just one.
   $(".crystal-image").on("click", function() {
+
+
 
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
@@ -71,34 +74,25 @@ function getRandomInt (min, max) {
     // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
 
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
+    // Display score and to start or if you win or loss
     $("#your-score").text(counter);
+    $("#gameResult").text(" ");
 
+    //if you win upcounter display you win and reset game
     if (counter === targetNumber) {
       winNum++;
       $("#wins").text(winNum);
-      targetNumber = getRandomInt (19,120);
-      newNumbers = numberOptions.sort(function() { return 0.5 - Math.random() }); 
-      assignCrystalValue(newNumbers);
-      console.log(newNumbers); 
-      console.log("imageCrystal: " + imageCrystal);  
-      counter=0;   
-      $("#number-to-guess").text(targetNumber);
-
-
+      gameReset();
+      $("#gameResult").text("You are a Winner!");
     }
 
+    //else if you lose to up loss counter, display and restart game
     else if (counter >= targetNumber) {
       lossNum++;
       $("#loss").text(lossNum);
-      targetNumber = getRandomInt (19,120);
-      newNumbers = numberOptions.sort(function() { return 0.5 - Math.random() });
-      assignCrystalValue(newNumbers);
-      console.log(newNumbers);
-      console.log("imageCrystal: " + imageCrystal); 
-      counter = 0; 
-      $("#number-to-guess").text(targetNumber);
-      
+      $("#gameResult").text("Sorry, You Lost! ");
+      gameReset();  
     }
-//    ($(".crystal-image1").attr("data-crystalvalue",20));
+//    
   });
+  gameReset();
